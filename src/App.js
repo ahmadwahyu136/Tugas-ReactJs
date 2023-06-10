b943af1b0090f91b452b5e67c52d52922d57ab07
ahmadwahyu136 <ahmadwahyuap@gmail.com>
2023-06-10 08:43:59 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
-import Homepage from './pages/Homepage';
-import LandingPage from './pages/LandingPage';
+import EventPage from './pages/EventPage';
 import { Route, Routes } from "react-router-dom"
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Homepage />} />
-      <Route path="/landingpage" element={<LandingPage />} />
+      <Route path="/" element={<EventPage/>} />
     </Routes>
   );
 }
